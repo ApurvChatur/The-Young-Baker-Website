2a4eb09ac238090ac7a73a7a7d44ed6b9217d67c
Apurv Chatur <chaturapurv@gmail.com>
2021-08-21 15:59:11 +0530

    Host Name

--- THE_YOUNG_BAKER/settings.py
+++ THE_YOUNG_BAKER/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9ijzsy(efx*66)n$=ioslzx6)-=-$7^wr^za6+=-re6c$n)mbp'
+SECRET_KEY = 'bae7a1a7e3ff3b31ce42fc9fd4011c7639ea6949c3fae727'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['theyoungbaker.herokuapp.com']
 
 
 # Application definition
